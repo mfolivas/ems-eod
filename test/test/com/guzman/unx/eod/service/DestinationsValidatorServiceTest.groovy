536d8e6f3e5efd0877b42bfd8778b685f2a3a6ea
mfolivas <mfolivas@gmail.com>
2015-04-09 01:15:56 -0400

    Remove hardcoded RDBMS and leverage embedded database.

--- test/test/com/guzman/unx/eod/service/DestinationsValidatorServiceTest.groovy
+++ test/test/com/guzman/unx/eod/service/DestinationsValidatorServiceTest.groovy
@@ -1,4 +1,7 @@
-package test.com.guzman.unx.eod.service;
+package test.com.guzman.unx.eod.service
+
+import groovy.sql.Sql
+import org.h2.tools.RunScript;
 
 import static org.junit.Assert.*
 
@@ -12,6 +15,8 @@ class DestinationsValidatorServiceTest {
 
 	@Test
 	public void should_have_no_unknown_destinations() {
+		def sql = Sql.newInstance("jdbc:h2:mem:intranet;MODE=MSSQLSERVER;IGNORECASE=true;", "sa", "sa", "org.h2.Driver")
+		RunScript.execute(sql.getConnection(), new FileReader("sql/master_detail_tables.sql"))
 		String filename = "test-files/20120626_Executions_US_1.csv"
 		String date = "20120626"
 		DateTime datetime = DateUtils.getDateTime(date)
--- test/test/com/guzman/unx/eod/trades/repository/SummaryRepositoryTest.groovy
+++ test/test/com/guzman/unx/eod/trades/repository/SummaryRepositoryTest.groovy
@@ -1,4 +1,8 @@
-package test.com.guzman.unx.eod.trades.repository;
+package test.com.guzman.unx.eod.trades.repository
+
+import org.h2.tools.RunScript
+import org.junit.Before
+import org.junit.BeforeClass;
 
 import static org.junit.Assert.*;
 
@@ -19,6 +23,13 @@ class SummaryRepositoryTest {
 	private static final String BASKET_FIVE = "JBPM-B-005"
 	private static final String BASKET_SIX = "JBWMC-S-006"
 	private static final String BASKET_SEVEN = "GE_CDP-S-007"
+
+	@Before
+	public void load_database() {
+		def sql = getConnection()
+		RunScript.execute(sql.getConnection(), new FileReader("sql/master_detail_tables.sql"))
+	}
+
 	
 	@Test
 	public void should_insert_only_records_that_are_not_in_trades() {
@@ -44,65 +55,49 @@ class SummaryRepositoryTest {
 	}
 	
 	
-	private def insert_records_in_detail_and_summary_and_return_source_with_totals(String tradedate, String fileName = "_Executions_US_1.csv") {
-		def sql = null
-		def map = [:]
-		try {
-			sql = Sql.newInstance("jdbc:sqlserver://gts10:1433;databaseName=intranet", "dbuser",
-				"bloody", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
-			
-			//delete and insert records in the details
-			String deleteDetails = "delete tbltradesdetail where tradedate = ? and ems_name = ?"
-			def itg = EMS.ITG
-			sql.execute(deleteDetails, [tradedate, itg.getName()])
-			
-			def date = DateUtils.getDateTime(tradedate)
-			
-			def service = new DetailsService()
-			service.process("test-files/"+tradedate+fileName, date.toDate(), itg)
-			
-			//Insert summary
-			
-			def delete = "DELETE tblTrades where TradeDate = ?"
-			sql.execute(delete,[tradedate])
-			
-			assert summaryRepository.insert(date.toDate(), EMS.ITG)
-			
-			def query = "select Source, COUNT(*) as total from tbltrades where TradeDate = ? group by Source order by source"
-			
-			sql.eachRow(query, [tradedate]) { record ->
-				println record
-				map[record.Source] = record.total
-			}
-		} catch (Exception e) {
-			throw e
-		} finally {
-			if(sql != null) {
-				sql.close()
-			}
-		} 
-		
-		return map		
-	}
-	
+
 	
 	private def remove_summary_and_insert_again(String tradedate, String basketName, String fileName = "_Executions_US_1.csv") {
-		insert_records_in_detail_and_summary_and_return_source_with_totals(tradedate, fileName)
-		def delete = "DELETE tblTrades where TradeDate = ? and source not in (?)"
-		
-		def sql = Sql.newInstance("jdbc:sqlserver://gts10:1433;databaseName=intranet", "dbuser",
-			"bloody", "com.microsoft.sqlserver.jdbc.SQLServerDriver")
-		sql.execute(delete,[tradedate, basketName])
-		
-		def date = DateUtils.getDateTime(tradedate)
-		assert summaryRepository.insert(date.toDate(), EMS.ITG)
-		
-		def query = "select Source, COUNT(*) as total from tbltrades where TradeDate = ? group by Source order by total"
 		def map = [:]
+		def sql = getConnection()
+		String deleteDetails = "delete tbltradesdetail where tradedate = ? and ems_name = ?"
+		def itg = EMS.ITG
+		sql.execute(deleteDetails, [tradedate, itg.getName()])
+
+		def date = DateUtils.getDateTime(tradedate)
+
+		def service = new DetailsService()
+		service.process("test-files/"+tradedate+fileName, date.toDate(), itg)
+
+		//Insert summary
+
+		def delete = "DELETE tblTrades where TradeDate = ?"
+		sql.execute(delete,[tradedate])
+
+		assert summaryRepository.insert(date.toDate(), EMS.ITG)
+
+		def query = "select Source, COUNT(*) as total from tbltrades where TradeDate = ? group by Source order by source"
+
 		sql.eachRow(query, [tradedate]) { record ->
 			map[record.Source] = record.total
 		}
+		def deleteBaskets = "delete tblTrades where TradeDate = ? and source not in (?)"
+
+
+		sql.execute(deleteBaskets,[tradedate, basketName])
+
+		assert summaryRepository.insert(date.toDate(), EMS.ITG)
+
+		def queryTotal = "select Source, COUNT(*) as total from tbltrades where TradeDate = ? group by Source order by total"
+		def filesWithTotals = [:]
+		sql.eachRow(queryTotal, [tradedate]) { record ->
+			filesWithTotals[record.Source] = record.total
+		}
 		sql.close()
-		return map
+		return filesWithTotals
+	}
+
+	private def getConnection() {
+		return Sql.newInstance("jdbc:h2:mem:intranet;MODE=MSSQLSERVER", "sa", "sa", "org.h2.Driver")
 	}
 }
